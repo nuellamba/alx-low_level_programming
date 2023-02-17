329fa3a58e11aa4f5ceac552f0a86cda3f4ccf44
Amba Nuella <nuellamba@yahoo.com>
2023-02-17 14:40:21 -0800

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+ * main - This is the main function
+ *
+ * Return: Should return just 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
+
