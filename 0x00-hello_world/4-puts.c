b703b572e9dcff19d372de261b8a8d906a2df158
Amba Nuella <nuellamba@yahoo.com>
2023-02-17 14:09:37 -0800

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-
 /**
  * main - This is the main function that puts a string
  *
- * Return: should return just 0
+ * Return: Should be 0
  */
+
 int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
