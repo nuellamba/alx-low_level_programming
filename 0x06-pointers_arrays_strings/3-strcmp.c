b479da593f0edbbe3c66f6a6267d48d2866d6da2
Amba Nuella <nuellamba@yahoo.com>
2023-03-02 21:40:27 -0800

    s

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _strcmp - This is the prototype to compare strings
+ *
+ * @s1: This is the first string to be compared
+ * @s2: This is the second string to be compared
+ * Return: Always 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
