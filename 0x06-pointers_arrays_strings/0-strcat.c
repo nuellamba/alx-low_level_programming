b8ca4371ea00396dde33673d08e28b7f641b311b
Amba Nuella <nuellamba@yahoo.com>
2023-03-02 20:58:02 -0800

    al

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,1 +1,22 @@
 #include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcat - This is the prototype function
+ *
+ * @dest: This is the first parameter
+ * @src: This is the second parameter
+ * Return: Always 0
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *ptr = dest;
+
+	for (; *dest; dest++)
+		;
+	for (; (*dest = *src); dest++, src++)
+		;
+	return (ptr);
+}
