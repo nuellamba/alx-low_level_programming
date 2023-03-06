6897736bc78045825cbe9128d3b7f6fd533ee780
Amba Nuella <nuellamba@yahoo.com>
2023-03-06 09:29:16 -0800

    str

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,18 @@
+#include "main.h"
 /**
- * strlen - find length of a string
- * @s: pointer to the string to chech
- * Return: void
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
  */
 int _strlen(char *s)
 {
-	int i = 0;
+	int longi = 0;
 
-	while (s[i])
-	i++;
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
 
-	return (i);
+	return (longi);
 }
